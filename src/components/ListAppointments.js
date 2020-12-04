5ef4de3e1d758c4df1e0a01b98e2f074e7e256ef
Travis Boss <travisboss@gmail.com>
2020-12-04 17:14:30 +0000

    add eslintcache to ignore

--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -8,7 +8,30 @@ class ListAppointments extends Component {
         <div>{item.ownerName}</div>
       </div>
     ));
-    return <div>{listItems}</div>;
+    return (
+      <div className="appointment-list item-list-mb-3">
+        {this.props.appointments.map((item) => (
+          <div className="pet-item col media py-3">
+            <div className="mr-3">
+              <button className="pet-delete btn btn-sm btn-danger">X</button>
+            </div>
+            <div className="pet-info media-body">
+              <div className="pet-head d-flex">
+                <span className="pet-name">{item.petName}</span>
+                <span className="apt-date ml-auto">{item.aptDate}</span>
+              </div>
+              <div className="owner-name">
+                <span className="label-item">
+                  <span>Owner: </span>
+                  {item.ownerName}
+                </span>
+              </div>
+              <div className="apt-notes">{item.aptNotes}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
